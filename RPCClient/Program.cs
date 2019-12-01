40446b3beb0e5c8e9dfa0b3553875033395f3fad
alanAval <alan@avaltech.com.br>
2019-11-30 22:06:38 -0300

    First Commit

---
+++ RPCClient/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPCClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rpc = new RpcClient();
+            Console.WriteLine("Digite g <SENSOR> para pegar o estado, " +
+                "s <SENSOR>/<ESTADO> para enviar um estado ou q para sair");
+            while (true)
+            {
+                var entry = Console.ReadLine();
+                entry = entry.Trim();
+                var command = entry.Substring(0, 1);
+                var parameter = entry.Substring(1, entry.Length - 1).Trim();
+                var result = "";
+                if (command == "g")
+                {
+                    result = rpc.GetState("get_state/" + parameter).ToString();
+                }else if (command == "s")
+                {
+                    result = rpc.SetState("set_state/" + parameter).ToString();
+                }
+                else if (command == "q")
+                {
+                    break;
+                }
+                Console.WriteLine("Resultado da chamada é " + result);
+            }
+            rpc.Close();
+        }
+    }
+}
---
+++ RPCClient/RcpClient.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+public class RpcClient
+{
+    private readonly IConnection connection;
+    private readonly IModel channel;
+    private readonly string replyQueueName;
+    private readonly EventingBasicConsumer consumer;
+    private readonly BlockingCollection<string> respQueue = new BlockingCollection<string>();
+    private readonly IBasicProperties props;
+
+    public RpcClient()
+    {
+        var factory = new ConnectionFactory() { HostName = "ec2-3-89-88-24.compute-1.amazonaws.com",
+        UserName = "dist",
+        Password = "dist"};
+
+        connection = factory.CreateConnection();
+        channel = connection.CreateModel();
+        replyQueueName = channel.QueueDeclare().QueueName;
+        channel.QueueBind(replyQueueName, "DIST", replyQueueName);
+        consumer = new EventingBasicConsumer(channel);
+
+        props = channel.CreateBasicProperties();
+        var correlationId = Guid.NewGuid().ToString();
+        props.CorrelationId = correlationId;
+        props.ReplyTo = replyQueueName;
+
+        consumer.Received += (model, ea) =>
+        {
+            var body = ea.Body;
+            var response = Encoding.UTF8.GetString(body);
+            if (ea.BasicProperties.CorrelationId == correlationId)
+            {
+                respQueue.Add(response);
+            }
+        };
+    }
+
+    public bool SetState(string message)
+    {
+        var messageBytes = Encoding.UTF8.GetBytes(message);
+        channel.BasicPublish(
+            exchange: "",
+            routingKey: "rpc_queue",
+            basicProperties: props,
+            body: messageBytes);
+
+        channel.BasicConsume(
+            consumer: consumer,
+            queue: replyQueueName,
+            autoAck: true);
+
+        return bool.Parse(respQueue.Take());
+    }
+
+    public decimal GetState(string message)
+    {
+        var messageBytes = Encoding.UTF8.GetBytes(message);
+        channel.BasicPublish(
+            exchange: "",
+            routingKey: "rpc_queue",
+            basicProperties: props,
+            body: messageBytes);
+
+        channel.BasicConsume(
+            consumer: consumer,
+            queue: replyQueueName,
+            autoAck: true);
+
+        return decimal.Parse(respQueue.Take().Replace('.',','));
+    }
+
+    public void Close()
+    {
+        connection.Close();
+    }
+}
